7f0fbd53fcbb8f8062ec777cb7a37d9020e1429d
Mukhit <78027029+Muha-tsokotukha@users.noreply.github.com>
2021-10-02 00:14:52 +0600

    1st task

---
+++ labs/lab3/1lab3.sql
@@ -0,0 +1,29 @@
+-- 1.
+-- a. Find all courses worth more than 3 credits;
+select * from course
+where credits>3;
+-- b. Find all classrooms situated either in ‘Watson’ or ‘Packard’ buildings;
+select * from classroom
+where (building='Watson' or building='Packard');
+-- c. Find all courses offered by the Computer Science department;
+select * from course
+where dept_name='Comp. Sci.';
+-- d. Find all courses offered during fall;
+select * from course,section
+where course.course_id=section.course_id and section.semester='Fall';
+-- e. Find all students who have more than 45 credits but less than 90;
+select * from student
+where (tot_cred>45 and tot_cred<90);
+-- f. Find all students whose names end with vowels;
+select * from student
+where (name like '%a' or
+name like '%e' or
+name like '%y' or
+name like '%u' or
+name like '%o' or
+name like '%i');
+-- g. Find all courses which have course ‘CS-101’ as their prerequisite;
+select * from course,prereq
+where course.course_id=prereq.course_id and prereq_id='CS-101';
+
+
